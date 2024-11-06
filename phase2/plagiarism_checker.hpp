460d3ba45c5e59e30078c3be38498d317936ad6f
drj_47 <dhruvrajmerchant.123@gmail.com>
2024-11-06 18:46:47 +0530

    newerrors

--- phase2/plagiarism_checker.hpp
+++ phase2/plagiarism_checker.hpp
@@ -23,8 +23,9 @@ public:
 protected:
     // TODO: Add members and function signatures here
     std::vector<std::pair<std::shared_ptr<submission_t>,std::chrono::time_point<std::chrono::system_clock>>> submissions;
-    static void check_plagiarism(std::shared_ptr<submission_t> submission1, std::shared_ptr<submission_t> submission2, bool flag_both = false);
+    void check_plagiarism(std::shared_ptr<submission_t> submission1, std::shared_ptr<submission_t> submission2, bool flag_both = false);
     std::vector<std::thread> threads;
     std::mutex mtx;
+    void individual_plag(std::shared_ptr<submission_t>submission,std::chrono::time_point<std::chrono::system_clock> curr_time);
     // End TODO
 };
